d4346f634fc5f4a7fbf0563f723dd45452c15621
Osmancordero <osmanpitufo112233@gmail.com>
2020-06-05 09:55:12 -0600

    se hicieron modificaciones en el index

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from datetime import date
 from datetime import datetime
 
 
-#instancia
+
 raiz = Tk()
 raiz.geometry("500x500")
 raiz.title("Ejecución de Funciones")
@@ -15,16 +15,16 @@ bienvenido.config(font=('Arial', 16))
 
 #--Funciones
 
-#--Caputara de datos en variable
+
 nombre = StringVar()
 apellido = StringVar()
 dia = IntVar()
 mes = IntVar()
 anio = IntVar()
 
-#--Convertir fecha a binario
+#binario(en desarrollo)
 
-#--Retorno de días Vividos
+#dias vividos
 def conteoDias():
     fechaString = f"{anio.get()}-{mes.get()}-{dia.get()}"
     date_object = datetime.strptime(fechaString, '%Y-%m-%d')
@@ -39,21 +39,20 @@ def conteoDias():
 
     lblResp.configure(text = respuesta)
 
-#--Conteo de Letras de Nombre / Apellido
+#par e impar de nombre y apellido
 def conteoLetras():
-    #--concatNombApelli = f"{nombre.get()}{apellido.get()}"
     sNombre = f"{nombre.get()}"
     sApellido = f"{apellido.get()}"
 
     conteoN = len(sNombre)
     conteoA = len(sApellido)
   
-#--Validacion Nombre
+
     if conteoN % 2 == 0:
         r1 = f"{sNombre} su Nombre es de número Par"
     else:
         r1 = f"{sNombre} su Nombre es de número Inpar"
-#--Validacion Apellido
+
     if conteoA % 2 == 0:
         r2 = f"{sApellido} su Apellido es de número Par."
     else:
@@ -64,32 +63,32 @@ def conteoLetras():
     lblResp.configure(text =respuesta )
 
 
-#--Conteo de Vocales y vocalesConsonantes
-#--Nombre al alReves
+#Conteo de Vocales y Consonantes
+#Nombre al alReves
 
 
-#--Nombre
+#Nombre
 lblnombre= Label(miFrame, text="Nombre:")
 lblnombre.grid(row=1, column=0)
 lblnombre.config(padx=10, pady=10)
 txtNombre=Entry(miFrame, textvariable =nombre)
 txtNombre.grid(row=1, column=1)
 
-#--Apellido
+#Apellido
 lblapellido=Label(miFrame, text="Apellido:")
 lblapellido.grid(row=2, column=0)
 lblapellido.config(padx=10, pady=10)
 txtApellido=Entry(miFrame, textvariable =apellido)
 txtApellido.grid(row=2, column=1)
 
-#--Día
+#Día
 lbldia=Label(miFrame, text="Día: ")
 lbldia.grid(row=3, column=0)
 lbldia.config(padx=10, pady=10)
 txtDia=Entry(miFrame, textvariable =dia)
 txtDia.grid(row=3, column=1)
 
-#--Mes
+#Mes
 lblmes=Label(miFrame, text="Mes: ")
 lblmes.grid(row=4, column=0)
 lblmes.config(padx=10, pady=10)
@@ -103,7 +102,7 @@ lblanio.config(padx=10, pady=10)
 txtanio=Entry(miFrame, textvariable =anio)
 txtanio.grid(row=5, column=1)
 
-#--Botones
+#Botones
 btnFuncion1 = Button(miFrame, text="Función 1")
 btnFuncion1.grid(row=6, column=0)
 btnFuncion1.config(padx=10, pady=10)
@@ -119,7 +118,7 @@ btnFuncion4.config(padx=10, pady=10)
 btnFuncion5 = Button(miFrame, text = "Función 5")
 btnFuncion5.grid(row=8, column=0)
 btnFuncion5.config(padx=10, pady=10)
-#--Respuesta
+#Respuesta
 lblResp=Label(miFrame, text="Respuesta:")
 lblResp.grid(row=9, column=0)
 lblResp.config(padx=10, pady=10)
